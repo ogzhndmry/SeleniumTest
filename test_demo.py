65259b7d437b743213849f14870bf8d04ec5fe67
oguzhan demiray <oguzhan@digitoygames.com>
2023-04-13 16:28:39 +0300

    test_valid_login methodunun eklenmesi

--- test_demo.py
+++ test_demo.py
@@ -23,7 +23,7 @@ class Test_DemoClass:
     #her testten sonra çağrılır
     def teardown_method(self):
         self.driver.quit()
-        
+
     #@mark.skip()
     @mark.parametrize("username,password",[("1","1"), ("kullaniciadim","sifrem")])
     def test_invalid_login(self,username,password):
@@ -37,6 +37,20 @@ class Test_DemoClass:
         errorMessage = self.driver.find_element(By.XPATH,"/html/body/div/div/div[2]/div[1]/div/div/form/div[3]/h3")
         self.driver.save_screenshot(f"{self.folderPath}/test-invalid-login-{username}-{password}.png")
         assert errorMessage.text == "Epic sadface: Username and password do not match any user in this service"
+
+    @mark.parametrize("username,password",[("standard_user","secret_sauce"), ("locked_out_user", "secret_sauce"), ("problem_user","secret_sauce")])
+    def test_valid_login(self,username,password):
+        self.driver.get("https://www.saucedemo.com/")
+        self.waitForElementVisible((By.ID,"user-name"))
+        usernameInput = self.driver.find_element(By.ID,"user-name")
+        passwordInput = self.driver.find_element(By.ID,"password")
+        # Action Chains
+        actions = ActionChains(self.driver)
+        actions.send_keys_to_element(usernameInput,username)
+        actions.send_keys_to_element(passwordInput,password)
+        actions.perform()
+        loginBtn = self.driver.find_element(By.ID,"login-button")
+        loginBtn.click()
 
     def waitForElementVisible(self,locator,timeout=5):
         WebDriverWait(self.driver,timeout).until(ec.visibility_of_element_located(locator))  
