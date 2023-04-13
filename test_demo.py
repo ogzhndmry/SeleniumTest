a1baf9ce403db1a2e6ceca619b377991b494103d
oguzhan demiray <oguzhan@digitoygames.com>
2023-04-13 16:11:28 +0300

    unusing import discards

--- test_demo.py
+++ test_demo.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-from time import sleep
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
